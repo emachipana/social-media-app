c5702549c4ce28b1de0de647385c0937afb7f51d
emachipana <emachipana0102@gmail.com>
2022-12-12 12:53:32 -0500

    ADD: render navbar from authenticated app

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,12 +1,17 @@
+import { Box } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
 
 function AuthenticatedApp() {
   return (
-    <Routes>
-      <Route index path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<h1>Home page</h1>} />
-      <Route path="/profile/:id" element={<h1>Profile page</h1>} />
-    </Routes>
+    <Box>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<h1>Home page</h1>} />
+        <Route path="/profile/:id" element={<h1>Profile page</h1>} />
+      </Routes>
+    </Box>
   )
 }
 
