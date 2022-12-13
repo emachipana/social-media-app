b4aff24620df8a14990d746eb7219e4b38aa0cf1
emachipana <emachipana0102@gmail.com>
2022-12-13 13:15:47 -0500

    FIX: move get user to home page

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,27 +1,9 @@
 import { Box } from "@mui/material";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import Navbar from "./scenes/navbar";
-import UserService from "./services/user";
-import { setUser } from "./state";
 
 function AuthenticatedApp() {
-  const { id } = useSelector((state) => state.user);
-  const token = useSelector((state) => state.token);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    async function fetch() {
-      const user = await UserService.getUser(token, id);
-
-      dispatch(setUser({user}));
-    }
-
-    fetch();
-  }, [dispatch, id, token]);
-
   return (
     <Box>
       <Navbar />
